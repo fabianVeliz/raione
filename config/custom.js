a32f136bbc5940d79d998204c9dc6020a356810b
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-04-30 22:46:28 -0300

    Update the app name and test email delivery

--- config/custom.js
+++ config/custom.js
@@ -60,10 +60,10 @@ module.exports.custom = {
 
   // The sender that all outgoing emails will appear to come from.
   fromEmailAddress: 'noreply@example.com',
-  fromName: 'The NEW_APP_NAME Team',
+  fromName: 'The Ration Team',
 
   // Email address for receiving support messages & other correspondences.
-  internalEmailAddress: 'support+development@example.com',
+  internalEmailAddress: 'velizfabianhoracio@gmail.com',
 
   // Whether to require proof of email address ownership any time a new user
   // signs up, or when an existing user attempts to change their email address.
