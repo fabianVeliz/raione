efe37de8082f8c592be987b2578bab93710fb473
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-05-01 23:32:47 -0300

    Add a simple modal to confirm a thing deletion

--- api/controllers/things/view-available-things.js
+++ api/controllers/things/view-available-things.js
@@ -17,15 +17,15 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
-    const friends = await User.findOne(this.req.me.id).populate('friends').friends;
-    const friendIds = _.pluck(friends, 'id');
+    const me = await User.findOne(this.req.me.id).populate('friends');
+    const friendIds = _.pluck(me.friends, 'id');
 
     const things = await Thing.find({
       or: [
         { owner: this.req.me.id },
         { owner: { in: friendIds } }
       ]
-    });
+    }).populate('owner');
 
     // Respond with view.
     return exits.success({
--- assets/js/pages/things/available-things.page.js
+++ assets/js/pages/things/available-things.page.js
@@ -3,7 +3,9 @@ parasails.registerPage('available-things', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    //…
+    things: [],
+    confirmDeleteThingModalOpen: false,
+    thingToDelete: null,
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -21,13 +23,31 @@ parasails.registerPage('available-things', {
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    clickThing: async function(id) {
-      console.log(`Click on the 'thing' with ID: ${id}`);
+    clickRemoveThing: function(thing) {
+      this.confirmDeleteThingModalOpen = true;
+      this.thingToDelete = thing;
+    },
+
+    clickCancelAction: function() {
+      this.confirmDeleteThingModalOpen = false;
+      this.thingToDelete = null;
+    },
+
+    clickConfirmAction: async function() {
+      const id = this.thingToDelete && this.thingToDelete.id;
+
+      if (!id) {
+        throw new Error('The thing id is missing.');
+      }
+
+      this.confirmDeleteThingModalOpen = false;
+      this.thingToDelete = null;
+
       await Cloud.destroyOneThing.with({ id });
       _.remove(this.things, { id });
       // We have to update this manually, because Vue does not detect
       // changes made by lodash.
       this.$forceUpdate();
-    }
+    },
   }
 });
